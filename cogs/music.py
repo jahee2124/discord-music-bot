970b65c64c4e17449a5a79d284d614b64a9c639a
jahee2124 <jahee2124@gmail.com>
2026-06-18 19:50:54 +0900

    유튜브 접근 형식 변경

--- cogs/music.py
+++ cogs/music.py
@@ -27,7 +27,7 @@ ytdl_format_options = {
     'compat_opts': ['no-youtube-unavailable-videos'],
     'extractor_args': {
         'youtube': {
-            'client': ['tv', 'android', 'web']
+            'client': ['mweb', 'default']
         }
     }
 }
